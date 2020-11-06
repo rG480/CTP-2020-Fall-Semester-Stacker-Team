d43613de884a33c747954460b754a864c2bbd0cc
Kenneth Hill <KHill0996@gmail.com>
2020-11-06 17:37:49 -0500

    Removed the data array

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -4,6 +4,8 @@ import React from 'react';
 class Item extends React.Component {
 
     render () {
+        
+
         return (
             <div className="col-lg-4 col-md-4 col-sm-4">
             <div className="card mb-3 shadow">
