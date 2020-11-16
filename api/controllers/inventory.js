0e511317ef7b17aa5c341330b1d5d303d8f0002d
rG480 <55470594+rG480@users.noreply.github.com>
2020-11-16 18:40:04 -0500

    asdf

--- api/controllers/inventory.js
+++ api/controllers/inventory.js
@@ -4,13 +4,7 @@ const db = require('../models');
 const { Inventory } = db;
 const passport =  require('../middlewares/authentication');
 router.get('/', (req,res) => {
-<<<<<<< HEAD
-    console.log(req.session.id);
-    console.log(req.session.cookie);
-    Inventory.findAll({where:{OwnerId:req.session.user}})
-=======
-    Inventory.findAll({where:{OwnerId: 1}, order: [[ 'createdAt' , 'DESC']]})
->>>>>>> 000fd7b4e3084d966bdd37a453786007248bba11
+    Inventory.findAll({where:{OwnerId: req.session.user}, order: [[ 'createdAt' , 'DESC']]})
     .then(inv => res.json(inv));
 });
 router.post('/',passport.isAuthenticated(), (req, res) => {
@@ -23,13 +17,8 @@ router.post('/',passport.isAuthenticated(), (req, res) => {
        purchasePrice: content.purchasePrice,
        currentPrice: content.currentPrice,
        description: content.description,
-<<<<<<< HEAD
        public:content.public })
       .then(item=>item.setOwner(req.session.user))
-=======
-       public: content.pub })
-      .then(item=>item.setOwner(1))
->>>>>>> 000fd7b4e3084d966bdd37a453786007248bba11
       .then(post => {
         res.status(201).json(post);
       })
