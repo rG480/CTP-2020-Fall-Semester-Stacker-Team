c8593ce683bb2f17b453c26c81c7e37b0d62424f
Kenneth Hill <KHill0996@gmail.com>
2020-11-17 17:15:37 -0500

    Wrote up the main idea for the about page. Once the front page and inventory pages are done I'll screenshot them and add them to the body.

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -3,7 +3,21 @@ import React from 'react';
 
 function AboutUsPage(props) {
   return (
-    <div>A little info about us.</div>
+    <div>
+      {/* Center All */}
+      {/* Bold , Larger Font*/}
+      Welcome to Cellar.
+
+      {/* Bold, Large Font */}
+      What is Cellar?
+
+      {/* Smaller font */}
+      Cellar is a basic inventory management system allowing you to track your collections and their details, including quantity, the price an item was purchased for, and the price of the item at the time it was added to your collection.abs
+
+      Cellar is a great way to track your items, with its simple, user friendly interface.
+         {/* Screenshot, float right or keep in center  */}
+      
+    </div>
     
   );
 }
