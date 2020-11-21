f626d3884124843d5246bdcc145bd88306393b7b
rG480 <55470594+rG480@users.noreply.github.com>
2020-11-20 19:51:56 -0500

    push

--- client/src/pages/PublicGalleryPage.js
+++ client/src/pages/PublicGalleryPage.js
@@ -1,14 +1,19 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
 class PublicGalleryPage extends React.Component {
     constructor(props){
       super(props)
       this.state={
-        showModal:false
+        
       }
-      this.toggler = this.toggleModal.bind(this)
+     
     }
   render(){
+   
+    alert(this.props.location.state.params.id1)
+    alert( this.props.location.state.params.id2)
     return (
-      <div>Yo buddy </div>
+      <div>Yo buddy</div>
     );
     
   }
