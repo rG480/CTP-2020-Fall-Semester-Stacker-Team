dd64af541e63d449cf63336031b3a8f5ac63bff0
cchromak <cchromak@gmail.com>
2020-11-25 15:46:07 -0500

    fit image in userinfPublic component

--- client/src/components/UserInfoPublic.js
+++ client/src/components/UserInfoPublic.js
@@ -7,6 +7,7 @@ import {
   NavLink,
   generatePath
 } from 'react-router-dom';
+
 class UserInfoPublic extends React.Component {
     constructor(props) {
         super(props)
@@ -16,9 +17,7 @@ class UserInfoPublic extends React.Component {
        return (
          <div style={{minWidth: "225px", maxWidth:"225px", padding: "15px"}}>
            <div className="card" >
-             <div className="card-body">
-             <Link to={{pathname:'/userGallery/',state: { params: {id1: this.props.list.userName, id2: this.props.list.userEmail} }}} className="btn btn-lite"><img style={{width:"100%",height:"100%"}}  className="card-img-top" src={this.props.list.imageURL} alt="userImage.jpg"></img></Link>
-             </div>
+             <Link to={{pathname:'/userGallery/',state: { params: {id1: this.props.list.userName, id2: this.props.list.userEmail} }}} className="btn btn-lite"><img style={{width:"100%",height:"100%", padding:"0px", margin:"0px",}}  className="card-img-top" src={this.props.list.imageURL} alt="userImage.jpg"></img></Link>
              <div className="card-footer small text-muted text-right">
                <h5> {this.props.list.userName }</h5>
             </div> 
