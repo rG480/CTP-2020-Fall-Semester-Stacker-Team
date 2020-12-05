899715af3536852d720520d2cdbb0ba085148f92
Kenneth Hill <KHill0996@gmail.com>
2020-12-05 10:07:44 -0500

    Added some stuff for the about page.

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -8,17 +8,21 @@ function AboutUsPage(props) {
     <div >
       {/* Center All */}
       {/* Bold , Larger Font*/}
-      Welcome to Cellar.
-
+      <h2 className="about-header"> <b>Welcome to Cellar</b></h2>
+      <br></br>
       {/* Bold, Large Font */}
-      What is Cellar?
-
-      {/* Smaller font */}
-      Cellar is a basic inventory management system allowing you to track your collections and their details, including quantity, the price an item was purchased for, and the price of the item at the time it was added to your collection.abs
-
+      <h3 className="about-sub-header"><b>What  is Cellar?</b></h3>
+      <p>
+      Cellar is a basic inventory management system allowing you to track your collections and their details, including quantity, the price an item was purchased for, and the price of the item at the time it was added to your collection.
+      <br></br>
       Cellar is a great way to track your items, with its simple, user friendly interface.
-         {/* Screenshot, float right or keep in center  */}
-        
+      <br></br>
+      Users can also browse the <a href="/users">collections of other users</a>.
+      <br></br>
+      
+      
+      
+      </p>
     </div>
     
   );
