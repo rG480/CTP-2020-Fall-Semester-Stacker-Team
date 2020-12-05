c6038c13965950187083b7ed2a39619f67d230d2
cchromak <cchromak@gmail.com>
2020-12-05 14:59:28 -0500

    added icon

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -5,20 +5,20 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 function AboutUsPage(props) {
   return (
-    <div >
+    <div style={{padding: "50px"}}>
       {/* Center All */}
       {/* Bold , Larger Font*/}
       <h2 className="about-header"> <b>Welcome to Cellar</b></h2>
       <br></br>
       {/* Bold, Large Font */}
       <h3 className="about-sub-header"><b>What  is Cellar?</b></h3>
-      <p>
-      Cellar is a basic inventory management system allowing you to track your collections and their details, including quantity, the price an item was purchased for, and the price of the item at the time it was added to your collection.
+      <p className="aboutUs"><br></br>
+      Cellar is a basic inventory management system allowing you to track your collections and 
+      <br></br>their details, including quantity, the price an item was purchased for, and the price of the 
+      <br></br>item at the time it was added to your collection. Cellar is a great way to track your items, with
+      <br></br>its simple, user friendly interface. Users can also browse the <a href="/users">collections of other users</a>.
       <br></br>
-      Cellar is a great way to track your items, with its simple, user friendly interface.
-      <br></br>
-      Users can also browse the <a href="/users">collections of other users</a>.
-      <br></br>
+      
       
       
       
